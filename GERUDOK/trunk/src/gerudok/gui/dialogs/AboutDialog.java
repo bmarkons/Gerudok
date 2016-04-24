d81f3be8ec2ece4cb350d1e47cc7d5b0bb5c3c2e
ra1-2013 <ra1-2013@2f573f69-0d56-c346-a060-0813b1c3ea79>
2016-04-24 13:45:07 +0000

    Dodat modifikovani About dijalog i lokalizovan u zavisnosti od izbora jezika.
    git-svn-id: https://student.ftn.uns.ac.rs/svn/sims/2016/tim1-1@73 2f573f69-0d56-c346-a060-0813b1c3ea79

--- GERUDOK/trunk/src/gerudok/gui/dialogs/AboutDialog.java
+++ GERUDOK/trunk/src/gerudok/gui/dialogs/AboutDialog.java
@@ -6,17 +6,19 @@ import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 
+import gerudok.gui.MainFrameGerudok;
+
 @SuppressWarnings("serial")
 public class AboutDialog extends JDialog {
 
 	public AboutDialog(Frame parent, String title, boolean modal) {
 		super(parent, title, modal);
-
-		JLabel background1 = new JLabel(new ImageIcon("images/about/about.jpg"));
+		
+		JLabel background1 = new JLabel(new ImageIcon(MainFrameGerudok.getInstance().getResourceBundle().getString("AboutImage")));
 		setResizable(false);
 		add(background1);
-
-		setSize(800, 480);
+		
+		setSize(770, 470);
 		setLocationRelativeTo(parent);
 	}
 }
